455aa8984719a8e79d58f0e8fce360b6d3a4b9f6
Tom Lijnse <tom.lijnse@layerzero.nl>
2019-02-05 17:07:03 +0100

    Added lab 9

---
+++ lab9/Addition.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+
+
+def addit(a, b):
+    c = a + b
+    return c
+
+print addit(10, 20)
---
+++ lab9/Rocks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+
+
+def rocks():
+    "Something witty"
+    print 'Arista Rocks!'
+
+for n in range(10):
+    rocks()
