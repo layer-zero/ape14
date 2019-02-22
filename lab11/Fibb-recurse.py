44b93e69d84d79f9af64a5ce98c9185dc238efee
Tom Lijnse <tom.lijnse@layerzero.nl>
2019-02-22 11:36:33 +0100

    Minor tweak to Fibb-recurse.py

--- lab11/Fibb-recurse.py
+++ lab11/Fibb-recurse.py
@@ -9,11 +9,9 @@ def fibonacci(n):
     elif n == 2:
         return [0, 1]
     else:
-        fib_list = fibonacci(n-1)
-        fib_list.append(fib_list[-2]+fib_list[-1])
-        return fib_list
+        fib = fibonacci(n-1)
+        fib.append(fib[-2] + fib[-1])
+        return fib
 
-fib = fibonacci(n)
-
-for f in fib:
+for f in fibonacci(n):
     print f
